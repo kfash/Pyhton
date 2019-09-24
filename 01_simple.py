9bcfad11a637c46581769991b066b14fe7ab514a
kfash <rafi2001@mail.ru>
2019-09-24 11:09:21 +0300

    Анимация (не работает onTimer())

--- 01_simple.py
+++ 01_simple.py
@@ -8,7 +8,23 @@ GR_SIMPLE - простые программы
 """
 from tkinter import *
 from graph import *
+import time
 
+def Fish (x, y):
+    c.create_arc(x, y, x + 500, y + 500,start=53, extent=90, style=ARC, outline='black', width=1)
+    c.create_arc(x+50, y - 50,x +150, y + 150, start=155, extent=50, style=ARC, outline='black', width=1)
+    c.create_arc(x, y - 400, x + 500, y +100, start=-53, extent=-90, style=ARC, outline='black', width=1)
+    
+def Update (obj1, obj2, obj3, dx, dy):
+	moveObjectBy(obj1, dx, dy)
+	moveObjectBy(obj2, dx, dy)
+	moveObjectBy(obj3, dx, dy)
+	
+def keyPressed(event):
+  if event.keycode == VK_ESCAPE:
+    close()  # закрыть окно 
+
+	
 c = canvas()
 c.pack()
 canvasSize(1000, 1800)
@@ -41,9 +57,9 @@ circle (700, 250, 150)
 penSize(1)
 brushColor("lightyellow")
 circle (700, 250, 50)
-c.create_arc(400, 1600, 900, 2100, start=53, extent=90, style=ARC, outline='black', width=1)
-c.create_arc(450, 1550,650, 1750, start=155, extent=50, style=ARC, outline='black', width=1)
-c.create_arc(400, 1200, 900, 1700, start=-53, extent=-90, style=ARC, outline='black', width=1)
+objp1 = c.create_arc(400, 1600, 900, 2100, start=53, extent=90, style=ARC, outline='black', width=1)
+objl2 = c.create_arc(450, 1550,650, 1750, start=155, extent=50, style=ARC, outline='black', width=1)
+objh3 = c.create_arc(400, 1200, 900, 1700, start=-53, extent=-90, style=ARC, outline='black', width=1)
 polyline([(370, 590), (420, 590), (460, 590), (500, 570)])
 rectangle(680, 100, 720, 400)
 rectangle(550, 230, 850, 270)
@@ -54,4 +70,10 @@ rectangle(100, 100, 300, 200)
 brushColor("yellow")
 polygon([(100,100), (200,50), 
          (300,100), (100,100)])
+onKey(keyPressed)
+onTimer(Update(objp1, objl2, objh3, 5, 0),50)
+'''while (True):
+    Update(objp1, objl2, objh3, 5, 0)
+    time.sleep(0.5)'''
+    
 run()
