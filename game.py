55c6c2607a35978755cf975189f9fe1b14f4a611
kfash <rafi2001@mail.ru>
2019-10-15 10:10:24 +0200

    Меню

--- game.py
+++ game.py
@@ -12,6 +12,8 @@ ball2 = [0, 0, 0, 0, 0, 0, 0]
 ball3 = [0, 0, 0, 0, 0, 0, 0]
 balls = [ball1, ball2, ball3]
 i = 0
+r = 40
+v = 10
 
 def new_ball ():
 	global balls
@@ -19,9 +21,9 @@ def new_ball ():
 	for i in range (0, 3):
 		balls[i][0] = rnd(100, 700)
 		balls[i][1] = rnd(100, 500)
-		balls[i][2] = rnd(30, 50)
-		balls[i][3] = rnd (-10, 10)
-		balls[i][4] = rnd (-10, 10)
+		balls[i][2] = rnd(r - 10, r + 10)
+		balls[i][3] = rnd (-v, v)
+		balls[i][4] = rnd (-v, v)
 		balls[i][5] = (c.create_oval(balls[i][0] - balls[i][2], balls[i][1] - balls[i][2], balls[i][0] + balls[i][2], balls[i][1] + balls[i][2], fill = choice(colors), width = 1))
 	root.after(3000, new_ball)
 
@@ -52,9 +54,25 @@ def move ():
 	root.after (10, move)
 
 	
-def change (event):
+def change1 (event):
 	global i
 	i = 1
+	
+def change2 (event):
+	global i
+	i = 2
+	
+def change21 (event):
+	global j
+	j = 1
+
+def change22 (event):
+	global j
+	j = 2
+
+def change23 (event):
+	global j
+	j = 3
 	
 def Prove ():
 	global i
@@ -63,19 +81,53 @@ def Prove ():
 		new_ball()
 		move()
 		prov()
+	if (i == 2):
+		j = 0
+		b1 = Button (root, text = " Легко")
+		b2 = Button (root, text = "Ноомально")
+		b3 = Button(root, text ="Сложно")
+		b1.pack()
+		b2.pack()
+		b3.pack()
+		b1.bind('<Button-21>', change21)
+		b2.bind('<Button-22>', change22)
+		b3.bind('<Button-23>', change23)
+		if (j == 1):
+			v = 7
+			r = 60
+		elif (j == 2):
+			v = 10
+			r = 40	
+		elif (j == 3):
+			v = 13
+			r = 20
 	else:
 		root.after (1, Prove)
 	
 def prov ():
 	c.bind('<Button-1>', click)
 	root.after (1, prov)
+	
+
+def wn1():
+    lst = root.grid_slaves()
+    for l in lst:
+        l.destroy()
+    root.geometry('512x256')
+    root.config(bg='GRAY')
+ 
 		
 
 e = Entry (width = 20)
-b1 = Button (root, text = " Начать")
+b1 = Button (root, text = " Начать", command=wn1)
+#b1.grid(row=0, column=0)
+b2 = Button (root, text = "Сложность", command=wn1)
+#b2.grid(row=0, column=0)
 e.pack()
 b1.pack()
-b1.bind('<Button-1>', change)
+b2.pack()
+b1.bind('<Button-1>', change1)
+b2.bind('<Button-2>', change2)
 
 Prove()
 root.mainloop()
